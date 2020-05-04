a3f09990fd0728395167608314009fbce10d384e
tony salloum <antoine.salloum@gmail.com>
2020-05-04 16:32:32 -0400

    axios call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      suggestedTime: [], 
+    }
+  }
+  componentDidMount() {
+    axios({
+      url: `https://api.sunrise-sunset.org/json`,
+      method: `GET`,
+      responseType: `json`,
+      params: {
+        lat: 43.6532,
+        lng: -79.3832,
+        date: 'tomorrow',
+      }
+    })
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          suggestedTime: response.data
+        })
+      });
+  }
+
   render() {
     return (
       <main>
