c251de083b944608a8890abf7455b5f7752abf67
tony salloum <antoine.salloum@gmail.com>
2020-05-08 15:14:58 -0400

    Updates

---
+++ precache-manifest.b96ba7d36d84885b78ab74b9549f2142.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def48f4b030c227bc04f8c5695d8705b",
+    "url": "/repo-name/index.html"
+  },
+  {
+    "revision": "4915ce06cd3af0aa6d52",
+    "url": "/repo-name/static/js/2.b1712c28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repo-name/static/js/2.b1712c28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de451d02f417fbb2bd19",
+    "url": "/repo-name/static/js/main.db779c91.chunk.js"
+  },
+  {
+    "revision": "8a66a66fcbddccbf6497",
+    "url": "/repo-name/static/js/runtime-main.07807b40.js"
+  }
+]);
