f807dedc4473b5cdf79f9181a00bbee607b4f28a
sarahmgans <sarah.gans@hotmail.com>
2020-05-08 14:32:34 -0400

    MVP

---
+++ src/Usertime.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Usertime = (props) => {
+  console.log(props);
+  return (
+    <main>
+      {
+      props.userTime}
+    </main>
+  );
+}
+
+export default Usertime;
