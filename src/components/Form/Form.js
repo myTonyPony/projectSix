b8d2e73abde17bb99c5e8dc8ae0450d19760d5b3
sarahmgans <sarah.gans@hotmail.com>
2020-05-09 18:54:54 -0400

    sass

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import './form.scss'
-
-
 
 function Form(props) {
   return (
--- src/components/UserTime/Usertime.js
+++ src/components/UserTime/Usertime.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./usertime.scss"
 
 const Usertime = (props) => {
   return (
