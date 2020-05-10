6338b63098927769ec0ec61cc3159f50522598cd
tony salloum <antoine.salloum@gmail.com>
2020-05-10 13:52:57 -0400

    reworked header to make it more flexy

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,19 +4,23 @@ class Header extends Component {
     render() {
         return (
             <header>
-                <div className="headerContainer">
+                <div className="headerContainer container">
                     <div className="headerContent">
                         <h1>Twilight Me</h1>
                     </div>
-                    <div className="infoButton dropdown" tabIndex="0">
-                        <i className="fas fa-info-circle" tabIndex="0"></i>
+                    <div className="hederInfo dropdown" tabIndex="0">
+                        <div className="infoButton">
+                            <i className="fas fa-info-circle" tabIndex="0"></i>
+                        </div>
                         <div className="dropdownContent" tabIndex="0">
                             <p>You can define twilight simply as the time of day between daylight and darkness, whether that's after sunset, or before sunrise. It's a time when the light from the sky appears diffused and often pinkish.</p>
                             <p> You want to experience this on your run! Let us twilight you: click on "go run!" below.</p>
                         </div>
+                        <div className="runButton">
+                            <a href="#form" className="goRun btn" onClick={this.clicked}>go run!</a>
+                        </div>
                     </div>
-                    <a href="#form" className="goRun btn" onClick={this.clicked}>go run!</a>
-                </div>
+                    </div>
             </header>
         );
     }
